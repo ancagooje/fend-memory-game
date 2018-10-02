73a453ac87ceb1f8bef4d639e9506d3fc48f7abc
Anca Gooje <39193827+ancagooje@users.noreply.github.com>
2018-10-02 17:39:50 -0400

    Update app.js

--- fend-project-memory-game-master/js/app.js
+++ fend-project-memory-game-master/js/app.js
@@ -12,14 +12,20 @@ cardHTML .join('');
  let cards = ['heart', 'heart', 'leaf', 'leaf', 'circle', 'circle', 'anchor', 'anchor', 'plane', 'plane', 'car', 'car', 'star', 'star', 'diamond', 'diamond'],
  
 
-/** 
-function generateCard(card) {
-    return '<li class="card" data-card= "${card}"><i class= "fa $(card)"></i></li>';
-}
-*/
 match = 0;
 moves = 0;
   
+function toggleCard(card) {
+    card.classList.toggle('open');
+    card.classList.toggle('show');
+
+}
+
+function addToggleCard(clickTarget) {
+    toggledCards.push(clickTarget);
+    console.log(toggledCards);
+}
+
  
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -37,7 +43,7 @@ function shuffle(array) {
 }
 
 /** I watched Yahya's tutorial, especially the part on matching cards, at : https://www.youtube.com/watch?v=G8J13lmApkQ  Used it as general inspiration.*/
- 
+/** 
 for(let i =0; i < cards.length; i++) {
     const card = document.createElement("li");
     card.classList.add("card");
@@ -45,21 +51,44 @@ for(let i =0; i < cards.length; i++) {
     cardsContainer.appendChild(card);
 
 }
+*/
+
 deck.addEventListener('click', event => {
     const clickTarget = event.target;
-    if (
-        clickTarget.classList.contains('card') &&
-        openCards.length < 2 &&
-        openCards.includes(clickTarget)
-    ) 
+    if (clickTarget.classList.contains('card') && toggledCards.length < 2) {
+        toggleCard(clickTarget);
+        addToggleCard(clickTarget);
+        if (toggledCards.length === 2) {
+            console.log('2 cards!');
+        }
+    }
+}); 
 
-    if (openCards.length === 2) {
+/** I read Matthew Cranford's tutorial on matching cards. https://matthewcranford.com/memory-game-walkthrough-part-3-matching-pairs/   */
+function checkForMatch() {
+    if (toggledCards[0].firstElementChild.className===
+       toggledCards[1].firstElementChild.className)
+       {
+           console.log('Match!');
+           toggledCards[0].classList.toggle('Match!');
+           toggledCards[1].classList.toggle('Match!');
+           toggledCards = [];
+       } else {
+           console.log('not a match!');
+          setTimeout(() => {
+          toggledCards(toggleCards[0]);
+          toggleCards(toggleCards[1]);
+           toggledCards = [];
+       }, 900);
+    }}
+
+    if (toggledCards.length === 2) {
         checkForMatch(clickTarget);}
-})
+
 
 //open only 2 cards at a time. after less than a second, flip cards back if not matched
 let allCards = document.querySelectorAll('.card');
-let openCards =[];
+let toggledCards =[];
  //add click Event
 allCards.forEach(function(card) {
     card.addEventListener('click', function(e) {        
@@ -68,6 +97,7 @@ openCards.push(this);
 console.log('Open Cards:', openCards.length);
     })
 })
+
 
 /**  Function for adding moves*/
 function addMove() {
